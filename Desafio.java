b0fa037f529e667c7891e6988c36a97378ccb27a
Lucas da Fonseca Alves <lucasfonsecalves@gmail.com>
2022-11-15 20:42:12 -0300

    Projeto Aprovado

---
+++ Desafio.java
@@ -0,0 +1,43 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.
+
+import java.util.*;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+
+        //TODO: Imprima se os valores numéricos passados são iguais ou não.
+
+        
+
+            Scanner scan = new Scanner(System.in);
+    
+            int A;
+            int B;
+
+            A = scan.nextInt();
+
+          
+            B = scan.nextInt();
+
+            if (A == B){
+                System.out.println("Sao iguais!");
+                
+            }
+            
+                else{
+                    
+               
+                System.out.println("Nao sao iguais!");
+            } 
+
+
+
+
+        
+    
+
+    }
+}
